4f303c46076dcaf841a55b38361abba69c6e6ce0
Daniel Berger <djberg96@gmail.com>
2017-01-21 05:40:50 -0700

    Added an 'add' method.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,8 @@
 class Deck < ApplicationRecord
   has_many :deck_cards
   has_many :cards, :through => :deck_cards
+
+  def add(card, quantity = 1)
+    deck_cards.create(:card => card, :quantity => quantity)
+  end
 end
