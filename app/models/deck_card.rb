9cd4c8309db3f5bb06fc46df45b64f48aa6d1608
Daniel Berger <djberg96@gmail.com>
2017-01-23 07:27:42 -0700

    Add a validation for quantity.

--- app/models/deck_card.rb
+++ app/models/deck_card.rb
@@ -3,4 +3,5 @@ class DeckCard < ApplicationRecord
   belongs_to :deck
 
   validates :deck_id, :uniqueness => {:scope => :card_id}
+  validates :quantity, :numericality => { :greater_than => 0, :less_than => 5 }
 end
