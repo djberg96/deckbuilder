fb2d72675d36c2987b38cc3630e674ceaa30158c
Daniel Berger <djberg96@gmail.com>
2020-08-27 08:03:10 -0400

    Modify the add method to just increase the quantity if it already exists.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,6 +3,13 @@ class Deck < ApplicationRecord
   has_many :cards, :through => :deck_cards
 
   def add(card, quantity = 1)
-    deck_cards.create(:card => card, :quantity => quantity)
+    if dc = deck_cards.find_by(:card_id => card.id)
+      dc.quantity += quantity
+      dc.save
+    else
+      deck_cards.create(:card => card, :quantity => quantity)
+    end
   end
+
+  alias add_card add
 end
