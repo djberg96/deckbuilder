e4d50c64adc52eb7b66203fa697820a06ef2af34
Daniel Berger <djberg96@gmail.com>
2020-08-27 07:23:46 -0400

    Add the add_to_deck method.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,8 @@
 class Card < ApplicationRecord
   has_many :deck_cards
   has_many :decks, :through => :deck_cards
+
+  def add_to_deck(deck, quantity = 1)
+    deck_cards.create(:card => self, :deck => deck, :quantity => quantity)
+  end
 end
