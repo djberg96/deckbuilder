4a01f3fccba150e26ddb21df3e8e2ef656f22168
Daniel Berger <djberg96@gmail.com>
2020-10-22 15:37:00 -0400

    Serialize and forward missing methods to the data hash.

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,14 @@ class Card < ApplicationRecord
   has_many :deck_cards
   has_many :decks, :through => :deck_cards
 
+  serialize :data
+  delegate_missing_to :data
+
   def add_to_deck(deck, quantity = 1)
     deck_cards.create(:card => self, :deck => deck, :quantity => quantity)
+  end
+
+  def data
+    @data ||= OpenStruct.new(self[:data])
   end
 end
