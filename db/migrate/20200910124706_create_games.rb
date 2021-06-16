188232a5d65a3bf7bf84c16fe741bf69dc0e90dc
Daniel Berger <djberg96@gmail.com>
2021-06-15 21:31:43 -0400

    Add maximum_individual_cards property to Game model.

--- db/migrate/20200910124706_create_games.rb
+++ db/migrate/20200910124706_create_games.rb
@@ -5,6 +5,7 @@ class CreateGames < ActiveRecord::Migration[5.2]
       t.text :description
       t.integer :maximum_cards_per_deck
       t.integer :minimum_cards_per_deck
+      t.integer :maximum_individual_cards
       t.timestamps
     end
   end
