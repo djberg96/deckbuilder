1c37a67590953b82be7113ccc2e5711bbfa8c749
Daniel Berger <djberg96@gmail.com>
2021-06-15 21:56:28 -0400

    Add maximum_individual_cards to schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_124721) do
     t.text "description"
     t.integer "maximum_cards_per_deck"
     t.integer "minimum_cards_per_deck"
+    t.integer "maximum_individual_cards"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
