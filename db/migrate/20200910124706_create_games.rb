562fe499a967bdcc886408bcb2340cb6a8a96014
Daniel Berger <djberg96@gmail.com>
2021-06-20 10:11:05 -0400

    Add data column to games table.

--- db/migrate/20200910124706_create_games.rb
+++ db/migrate/20200910124706_create_games.rb
@@ -6,6 +6,7 @@ class CreateGames < ActiveRecord::Migration[5.2]
       t.integer :maximum_cards_per_deck
       t.integer :minimum_cards_per_deck
       t.integer :maximum_individual_cards
+      t.json :data
       t.timestamps
     end
   end
