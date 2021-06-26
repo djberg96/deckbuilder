3a0c362e9900148892d70c0fb14879b407cabc94
Daniel Berger <djberg96@gmail.com>
2021-06-26 00:41:34 -0400

    Fix the Card#quantity method, add some comments.

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,14 +5,21 @@ class Card < ApplicationRecord
   serialize :data
   delegate_missing_to :data
 
+  # Add cards to the specified deck. If no quantity is specified, then one
+  # is the default.
+  #
   def add_to_deck(deck, quantity = 1)
     deck_cards.create(:card => self, :deck => deck, :quantity => quantity)
   end
 
+  # Find the quantity of a given card for the current deck.
+  #
   def quantity
-    deck_cards.find(self.id).quantity
+    deck_cards.find_by(:card_id => self.id).quantity
   end
 
+  # Return the JSON data as an openstruct object.
+  #
   def data
     @data ||= OpenStruct.new(self[:data])
   end
