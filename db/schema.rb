7cb50531b06100a26c5c94233b9498da8fc84f8b
Daniel Berger <djberg96@gmail.com>
2021-06-27 22:49:44 -0400

    Add a private column to the decks table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_205327) do
+ActiveRecord::Schema.define(version: 2021_06_28_024814) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_06_27_205327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "private", default: false
   end
 
   create_table "game_decks", force: :cascade do |t|
