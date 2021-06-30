4c7e601e590412308cb7c46c5977c8a4774462f5
Daniel Berger <djberg96@gmail.com>
2021-06-30 16:18:40 -0400

    Add some message parsing.

--- lib/loggers/database_logger.rb
+++ lib/loggers/database_logger.rb
@@ -3,6 +3,7 @@ module Loggers
     # Set logdev to nil to prevent it from writing to a file.
     def initialize(logdev = nil, *args)
       super(logdev, *args)
+      @regex = /(.*?)(\(.*?\))/
     end
 
     # Redefine the add method from the core Logger class so that it
@@ -16,6 +17,7 @@ module Loggers
       end
 
       return unless message
+      parts = parse_message(message)
 
       # Prevent the Log.create call from logging itself, which
       # otherwise causes an infinite loop.
@@ -26,6 +28,13 @@ module Loggers
           :timestamp => Time.now,
         )
       end
+    end
+
+    def parse_message(msg)
+      match = @regex.match(msg)
+      captures = match.captures
+      main_message = captures.first.strip
+      message_info = m.captures.last.split("|").map{ |e| e.tr("()", "").strip.split(":").map(&:strip) }.to_h
     end
 
     def map(severity)
