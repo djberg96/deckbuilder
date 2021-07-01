cf79ebc46d4145f83ba0822e8510cff34f6adbca
Daniel Berger <djberg96@gmail.com>
2021-06-30 23:38:09 -0400

    Update group model, add handy methods for users and groups.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ApplicationRecord
-  has_many :users, :through => :user_group
+  has_many :user_groups
+  has_many :users, :through => :user_groups
+
+  def add_user(user)
+    user_groups.create(:user => user, :group => self)
+  end
 end
