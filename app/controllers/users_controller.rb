d7d43dd0d1071b912231b5954145407130db1fc9
Daniel Berger <djberg96@gmail.com>
2021-07-02 22:22:13 -0400

    Add some data to index views, modify user list controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.where(:private => false)
   end
 
   # GET /users/1 or /users/1.json
