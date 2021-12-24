65939a69e67bf4966529083a52f431be889e3c78
Daniel Berger <djberg96@gmail.com>
2021-12-24 07:52:16 -0500

    Add username validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   has_many :user_groups
   has_many :groups, :through => :user_groups
 
+  validates :username, :length => {:minimum => 3, :maximum => 16}, :allow_blank => false
+
   def add_to_group(group)
     user_groups.create(:user => self, :group => group)
   end
