e49609c2483903118826206f6a3aca3abcccc0c5
Daniel Berger <djberg96@gmail.com>
2021-12-24 07:46:48 -0500

    Whitespace.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-       params.require(:user).permit(
+      params.require(:user).permit(
         :first_name,
         :last_name,
         :username,
