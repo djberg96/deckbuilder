afd4e2600717f94abb3bf6069d687d0cc28fabcf
Daniel Berger <djberg96@gmail.com>
2022-02-22 07:33:26 -0500

    Add some validations to the game model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,6 +3,26 @@ class Game < ApplicationRecord
   has_many :decks, :through => :game_decks
   has_many :cards
 
+  validates :minimum_individual_cards,
+    :numericality => {
+      :only_integer          => true,
+      :greater_than          => 0,
+      :less_than_or_equal_to => :maximum_cards_per_deck,
+    }
+
+  validates :maximum_individual_cards,
+    :numericality => {
+      :only_integer             => true,
+      :greater_than             => 0,
+      :greater_than_or_equal_to => :minimum_cards_per_deck,
+    }
+
+  validates :maximum_individual_cards,
+    :numericality => {
+      :only_integer             => true,
+      :greater_than             => 0,
+    }
+
   def add_deck(deck, quantity = 1)
     game_decks.create(:deck => deck, :quantity => quantity)
   end
