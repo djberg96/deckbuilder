c6b065f70902185b821dd32311ddf97da0ebd1f4
Daniel Berger <djberg96@gmail.com>
2022-02-23 06:24:23 -0500

    Update authorize to actually set @user, and update views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    unless User.find_by(:id => session[:user_id])
-      redirect_to login_url, :notice => "Please log in"
+    unless @user = User.find_by(:id => session[:user_id])
+      redirect_to login_url, :notice => "Please log in!"
     end
   end
 end
