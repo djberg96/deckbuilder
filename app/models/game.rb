a074dea88e48768a747246610bb421009f8d86db
Daniel Berger <djberg96@gmail.com>
2022-06-14 12:39:55 -0400

    Fix validation on Game model, remove duplicate validation.

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,24 +3,11 @@ class Game < ApplicationRecord
   has_many :decks, :through => :game_decks
   has_many :cards
 
-  validates :minimum_individual_cards,
+  validates :maximum_individual_cards,
     :numericality => {
       :only_integer          => true,
       :greater_than          => 0,
-      :less_than_or_equal_to => :maximum_cards_per_deck,
-    }
-
-  validates :maximum_individual_cards,
-    :numericality => {
-      :only_integer             => true,
-      :greater_than             => 0,
-      :greater_than_or_equal_to => :minimum_cards_per_deck,
-    }
-
-  validates :maximum_individual_cards,
-    :numericality => {
-      :only_integer             => true,
-      :greater_than             => 0,
+      :less_than_or_equal_to => :minimum_cards_per_deck,
     }
 
   def add_deck(deck, quantity = 1)
