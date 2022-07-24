4fb4d683c135342b867c8e1fd16069fc56ea1eb5
Daniel Berger <daniel.berger@covermymeds.com>
2022-07-24 11:15:59 -0400

    Move sessions controller tests to rspec.

--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'rails_helper'
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+RSpec.describe "Sessions", type: :request do
+  example "should get new" do
     get sessions_new_url
     assert_response :success
   end
 
-  test "should get create" do
+  example "should get create" do
     get sessions_create_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  example "should get destroy" do
     get sessions_destroy_url
     assert_response :success
   end
