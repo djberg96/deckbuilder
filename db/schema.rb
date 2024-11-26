5c79ad7e58f0c31eb20add63d0b4237669241b59
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-11-26 15:21:39 -0500

    Add edition column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_26_181231) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_26_201134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_26_181231) do
     t.json "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "edition"
   end
 
   create_table "groups", force: :cascade do |t|
