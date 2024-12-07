e15b9c2d0fd52f31d5f4a5da1c8c915739860ca5
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-12-07 15:06:09 -0500

    Username should be unique.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
   has_many :user_groups
   has_many :groups, :through => :user_groups
 
-  validates :username, :length => {:minimum => 3, :maximum => 16}, :allow_blank => false
+  validates :username,
+    :length      => {:minimum => 3, :maximum => 16},
+    :allow_blank => false,
+    :uniqueness  => true
+
   validates :password, :confirmation => true
 
   def add_to_group(group)
