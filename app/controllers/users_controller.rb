6ae97f0896a54129f3d49535ca4997585c2890b8
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-01-25 06:12:43 -0500

    Don't authorize on create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  before_action :authorize, except: :new
+  before_action :authorize, except: [:new, :create]
 
   # GET /users or /users.json
   def index
