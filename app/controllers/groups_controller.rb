bfc97d7491b1f4dc8b0cd31aa7cc70c90c827ad2
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-01-25 10:22:35 -0500

    Update new screen for groups so you can add users.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -64,6 +64,6 @@ class GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.require(:group).permit(:name, :description, :private)
+      params.require(:group).permit(:name, :description, :private, user_ids: [])
     end
 end
